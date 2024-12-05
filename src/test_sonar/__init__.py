ecc9e771069e8a0150629603f5fe52c50226b5e2
Maximilian Linhoff <maximilian.linhoff@tu-dortmund.de>
2024-12-05 16:36:17 +0100

    Basic structure

---
+++ src/test_sonar/__init__.py
@@ -0,0 +1,7 @@
+def fib(n):
+    if n < 0:
+        raise ValueError(f"n must be >=0, got {n}")
+    if n < 2:
+        return n
+
+    return fib(n - 1) + fib(n - 2)
---
+++ src/test_sonar/test_fib.py
@@ -0,0 +1,18 @@
+import pytest
+
+
+FIBS = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144]
+
+@pytest.mark.parametrize(("n", "expected"), enumerate(FIBS))
+def test_fib(n, expected):
+    from test_sonar import fib
+    assert fib(n) == expected
+
+def test_negative():
+    from test_sonar import fib
+
+    with pytest.raises(ValueError, match="n must be >=0, got -1"):
+        fib(-1)
+
+    with pytest.raises(ValueError, match="n must be >=0, got -100"):
+        fib(-100)
